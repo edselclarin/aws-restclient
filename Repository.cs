7a81fa97e445bd8bee513437399604ae731f1beb
Edsel Clarin <edsel.clarin@yahoo.com>
2017-10-10 22:36:06 -0700

    Added Version 1.1 files via Upload
    Version 1.1 outputs a page that lists all the repositories found at https://api.github.com/orgs/dotnet/repos.

---
+++ Repository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace restclient
+{
+    [DataContract(Name = "repo")]
+    public class Repository
+    {
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+        /*  // The line of code above is equivalent to block of code below.
+            public string Name 
+            { 
+                get { return this._name; }
+                set { this._name = value; }
+            }
+            private string _name;         
+        */
+
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+
+        [DataMember(Name = "html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+        [DataMember(Name = "homepage")]
+        public Uri Homepage { get; set; }
+
+        [DataMember(Name = "watchers")]
+        public int Watchers { get; set; }
+
+        [DataMember(Name = "pushed_at")]
+        private string JsonDate { get; set; }
+
+        // IgnoreDataMember instructs the serializer that this type should not
+        // be read to or written from any JSON object.
+        [IgnoreDataMember]
+        public DateTime LastPush
+        {
+            get
+            {
+                return DateTime.ParseExact(
+                    JsonDate, "yyyy-MM-ddTHH:mm:ssZ",
+                    CultureInfo.InvariantCulture);
+            }
+
+            // No set accessor, means read-only property.
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"{Name}{Environment.NewLine}{Description}{Environment.NewLine}{GitHubHomeUrl}{Environment.NewLine}{Homepage}{Environment.NewLine}{Watchers}{Environment.NewLine}{LastPush}{Environment.NewLine}");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
 
 namespace restclient
 {
@@ -9,8 +15,45 @@ namespace restclient
         {
             app.Run(context =>
             {
-                return context.Response.WriteAsync("Hello from ASP.NET Core!");
+                string strOutput = "List of repos at https://api.github.com/orgs/dotnet/repos:" + Environment.NewLine;
+                strOutput += Environment.NewLine;
+
+                var repositories = ProcessRepositories().Result;
+                foreach (var repo in repositories)
+                    strOutput += repo.ToString() + Environment.NewLine;
+                strOutput += Environment.NewLine;
+                strOutput += "Coded by Edsel Clarin.";
+
+                return context.Response.WriteAsync(strOutput);
             });
+        }
+
+        private static async Task<List<Repository>> ProcessRepositories()
+        {
+            var client = new HttpClient();
+
+            // Reset headers.
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            // Configure to accept JSON response.
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue(
+                    "application/vnd.github.v3+json"));
+
+            // Add the User Agent.
+            client.DefaultRequestHeaders.Add(
+                "User-Agent", ".NET Foundation Repository Reporter");
+
+            // Make the web request, and deserialize the response into a list 
+            // of respositories.
+            var serializer = new DataContractJsonSerializer(
+                typeof(List<Repository>));
+            var streamTask = client.GetStreamAsync(
+                "https://api.github.com/orgs/dotnet/repos");
+            var repositories = serializer.ReadObject(await streamTask)
+                as List<Repository>;
+
+            return repositories;
         }
     }
 }
